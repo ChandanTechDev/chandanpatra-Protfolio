0a2ac0ead2777dd906e326455ba30dd36adc47de
Chandan Patra <chandanpatra144@gmail.com>
2024-11-15 17:36:45 +0530

    commit changes message

--- src/constants/data.js
+++ src/constants/data.js
@@ -9,50 +9,50 @@ export const menuLink = [
   },
 ];
 
-export const projects = [
-  {
-    id: 1,
-    title: "BLOG-VISTA",
-    description:
-      "A blog app is a web app that lets users write and publish blog posts. Bloggers use the app to create posts, edit posts, and submit posts for review.",
-    image:
-      "https://res.cloudinary.com/dyxt9lvpu/image/upload/v1715529913/c4bmhw0rhkh0gkiu4avl.png",
-    tech: ["React.js", "Node.js", "Express.js", "Mongodb", "Tailwind css"],
-    liveLink: "https://splendid-bublanina-8aa0fb.netlify.app",
-    githubLink: "https://github.com/TUHINKAPRI/Blog-Vista-2",
-    icon: "blogging.png",
-  },
-  {
-    id: 2,
-    title: "STUDY-SPHERE",
-    description:
-      "A platform for posting courses which are designed as per the Industry demand with full Career Assistance.",
-    image:
-      "https://res.cloudinary.com/dyxt9lvpu/image/upload/v1715529829/mycykyys5hs4izr7ycul.png",
-    tech: ["Next.js", "Node.js", "Express.js", "Mongodb", "Tailwind css"],
-    liveLink: "https://studysphere11.netlify.app/",
-    githubLink: "https://github.com/TUHINKAPRI/course-create-app",
-    icon: "studying.png",
-  },
-];
+// export const projects = [
+//   {
+//     id: 1,
+//     title: "BLOG-VISTA",
+//     description:
+//       "A blog app is a web app that lets users write and publish blog posts. Bloggers use the app to create posts, edit posts, and submit posts for review.",
+//     image:
+//       "https://res.cloudinary.com/dyxt9lvpu/image/upload/v1715529913/c4bmhw0rhkh0gkiu4avl.png",
+//     tech: ["React.js", "Node.js", "Express.js", "Mongodb", "Tailwind css"],
+//     liveLink: "https://splendid-bublanina-8aa0fb.netlify.app",
+//     githubLink: "https://github.com/TUHINKAPRI/Blog-Vista-2",
+//     icon: "blogging.png",
+//   },
+//   {
+//     id: 2,
+//     title: "STUDY-SPHERE",
+//     description:
+//       "A platform for posting courses which are designed as per the Industry demand with full Career Assistance.",
+//     image:
+//       "https://res.cloudinary.com/dyxt9lvpu/image/upload/v1715529829/mycykyys5hs4izr7ycul.png",
+//     tech: ["Next.js", "Node.js", "Express.js", "Mongodb", "Tailwind css"],
+//     liveLink: "https://studysphere11.netlify.app/",
+//     githubLink: "https://github.com/TUHINKAPRI/course-create-app",
+//     icon: "studying.png",
+//   },
+// ];
 
 export const educationData = [
   {
-    name: "B.Tech , Electronics and Communication Engineering ",
-    institute: "MCKV Institute of Engineering",
-    time: "2019-2023",
-    marks: "CGPA - 8.7",
+    name: "BCA , Bechelar of Computer Application",
+    institute: "Panskura Banamali College",
+    time: "2018-2021",
+    marks: "CGPA - 7.9",
   },
   {
     name: "XII , 12th",
-    institute: "Rasikganja Vidyasagar High School",
+    institute: "Belda Gangadhar Academy",
     time: "2017-2018",
-    marks: "73%",
+    marks: "70%",
   },
   {
     name: "X , 10th",
-    institute: "Rasikganja Vidyasagar High School",
+    institute: "Jankapur High School",
     time: "2015-2016",
-    marks: "66%",
+    marks: "63%",
   },
 ];
