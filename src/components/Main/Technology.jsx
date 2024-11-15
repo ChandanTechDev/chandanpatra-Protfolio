b2ab903e6b57e60fc8ada4895dc94775b134a748
Chandan Patra <chandanpatra144@gmail.com>
2024-11-15 18:09:46 +0530

    commit2 message

--- src/components/Main/Technology.jsx
+++ src/components/Main/Technology.jsx
@@ -9,7 +9,7 @@ function Technology() {
         </h1>
       </div>
       <div className="flex flex-wrap justify-center gap-6">
-        <img src="wordpress-logo.svg" alt="" className="w-12 h-12 " />
+        <img src="wordpress.png" alt="" className="w-12 h-12 " />
         <img src="825519.png" alt="" className="w-12 h-12 " />
         <img src="php-logo.svg" alt="" className="w-12 h-12 " />
         <img src="html.png" alt="" className="w-12 h-12 " />
